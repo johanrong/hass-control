723073496d54a1c5629d6de26976633f6be8e802
Johan Rong <59508244+johainworks@users.noreply.github.com>
2023-10-29 21:25:45 +0100

    Building layout and planning

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,10 @@ import {
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+import { apps, bulb, thermometer } from 'ionicons/icons';
+import Overview from './pages/Overview';
+import Lights from './pages/Lights';
+import Temperature from './pages/Temperature';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -42,13 +42,13 @@ const App: React.FC = () => (
       <IonTabs>
         <IonRouterOutlet>
           <Route exact path="/tab1">
-            <Tab1 />
+            <Overview />
           </Route>
           <Route exact path="/tab2">
-            <Tab2 />
+            <Lights />
           </Route>
           <Route path="/tab3">
-            <Tab3 />
+            <Temperature />
           </Route>
           <Route exact path="/">
             <Redirect to="/tab1" />
@@ -56,16 +56,16 @@ const App: React.FC = () => (
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon aria-hidden="true" icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+            <IonIcon aria-hidden="true" icon={apps} />
+            <IonLabel>Overview</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon aria-hidden="true" icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+            <IonIcon aria-hidden="true" icon={bulb} />
+            <IonLabel>Lights</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon aria-hidden="true" icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+            <IonIcon aria-hidden="true" icon={thermometer} />
+            <IonLabel>Temperature</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
